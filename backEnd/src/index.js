625cc6e6f26016fda1f03b6b0d7d806a5cc1dab2
Kushal Vardhan <vardhank140102@gmail.com>
2025-10-28 21:08:50 +0530

    basic server created

---
+++ backEnd/src/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import routes from "./routes/cart.routes.js"
+import db from "./utils/db.js"
+
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 4000
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+db()
+
+app.use("/api", routes)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+
+})
