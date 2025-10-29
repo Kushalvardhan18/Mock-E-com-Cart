bada084ed5da97d3e2d367eb09be85f22162c9af
Kushal Vardhan <vardhank140102@gmail.com>
2025-10-29 11:27:57 +0530

    cors from all origins

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -8,8 +8,10 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 4000
 app.use(express.json())
-app.use(cors(
-))
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // allow all origins
+  next();
+});
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
