bc52c6619f59bb857e750c78f5a783e35931865a
Kushal Vardhan <vardhank140102@gmail.com>
2025-10-29 10:57:52 +0530

    changes in db

--- backEnd/src/utils/db.js
+++ backEnd/src/utils/db.js
@@ -4,15 +4,16 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const db = async () => {
-await mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
- .then(() => {
-        console.log("Connected to MongoDB")
-    })
-    .catch((err) => {
-        console.error("Error:", err)
-    })
-}
-export default db
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // optional, default is 30s
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
+export default db;
