f577682f03cc29d220fae9c2bb24853a91817a9b
Kushal Vardhan <vardhank140102@gmail.com>
2025-10-29 12:59:18 +0530

    items maping complete

--- frontEnd/src/components/Products.jsx
+++ frontEnd/src/components/Products.jsx
@@ -1,38 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import apiClient from "../../service/apiClient";
 
 const Products = () => {
+  const [data, setData] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await apiClient.products();
-        console.log(data);
+        const response = await apiClient.products();
+        console.log(response);
+
+        setData(response);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
   }, []);
+  if (!data) return <h1>loading...</h1>;
+
+ const items = data.products?.map((product) => ({
+    id: product._id,
+    name: product.name,
+    price: product.price,
+    image: product.image,
+  })) || [];
   return (
     <>
       <h1 className="text-center text-3xl">Products</h1>
       <div className="grid grid-cols-4 gap-5 m-3">
-        <div>
-          <img
-            src="https://imgs.search.brave.com/GfrFxjRhqQFPwPP50tHh8McLLaEaKLCrCRav_WSeJ9c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi95ZWxs/b3ctY3Jhc2gtdGVz/dC1kdW1teS1wb3Np/dGlvbmVkLWhhcm5l/c3MtcmVhZHktc2Fm/ZXR5LWV2YWx1YXRp/b25zLWluc2lkZS1t/b2Rlcm4tdGVzdGlu/Zy1mYWNpbGl0eS1i/cmlnaHQtMzY2MTI5/OTQ2LmpwZw"
-            alt=""
-          />
-          <div className="flex justify-between">
-            <h3>robot</h3>
-            <h3>2000</h3>
+        {items.map((item) => (
+          <div key = {item.id} className="bg-white p-1">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-70 object-fill"
+            />
+            <div className="flex justify-between mx-1 mb-8">
+              <h3 className="text-black font-bold">{item.name}</h3>
+              <h3 className="text-blue-900 font-semibold"> ₹ {item.price}</h3>
+            </div>
+            <div className="flex justify-center">
+              <button className="bg-blue-500 py-2 px-3 rounded-md cursor-pointer hover:bg-blue-800 hover:scale-110 duration-500">
+                Add to cart
+              </button>
+            </div>
           </div>
-          <div className="flex justify-center">
-            <button className="bg-blue-500 py-2 px-3 rounded-md cursor-pointer hover:bg-blue-800 hover:scale-110 duration-500">
-              Add to cart
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
